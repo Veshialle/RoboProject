36b293a215a003c6b706775a30b4e0f2c0828052
ciurga93 <big-g@live.it>
2015-12-01 12:06:23 +0100

    v5
    aggiunta gestione link visitati

--- Main.py
+++ Main.py
@@ -3,48 +3,59 @@ from bs4 import BeautifulSoup
 
 
 f1 = open('results.txt', 'w')
-f2 = open('sources.txt', 'r+')
+f2 = open('source.txt', 'r')
+f3 = open('seen.txt', 'a+')
 links = []
+visitedlinks = []
 
-for url in f2.readlines():
-    links.append(url)
 
 def crawler(maxite):
-    count = 1
-    if count == 1:
+    count = 0
+    if count == 0:
         f1.write("---------- VISITED VIDEOS ----------\n\n")
     for url in links:
-        #print(url)
         if count == maxite:
-            print("done!")
+            print("Done! I visited " + str(maxite) + " videos")
             break
-        source_code = requests.get(str(url))
-        plain_text = source_code.text
-        soup = BeautifulSoup(plain_text, "html.parser")
-        for title in soup.findAll("span", {'id': 'eow-title'}, {'class': 'watch-title'}):
-            title1 = title.get('title')
-            print(title1)
-            f1.write("Title: " + title1 + "\n")
+        if (url + "\n") not in visitedlinks:
+            print(visitedlinks)
+            visitedlinks.append(url)
+            f3.write(url)
+            f3.write("\n")
+            source_code = requests.get(str(url))
+            plain_text = source_code.text
+            soup = BeautifulSoup(plain_text, "html.parser")
+            for title in soup.findAll("span", {'id': 'eow-title'}, {'class': 'watch-title'}):
+                title1 = title.get('title')
+                print(title1)
+                f1.write("Title: " + title1 + "\n")
 
-        for date in soup.findAll("strong", {'class': 'watch-time-text'}):
-            date1 = date.string
-            print(date1)
-            f1.write("date: " + date1 + "\n")
+            for date in soup.findAll("strong", {'class': 'watch-time-text'}):
+                date1 = date.string
+                f1.write("date: " + date1 + "\n")
 
-        for views in soup.findAll("div", {'class': 'watch-view-count'}):
-            views1 = views.string
-            print(views1)
-            f1.write("Views: " + views1 + "\n")
-        f1.write("\n \n")
+            for views in soup.findAll("div", {'class': 'watch-view-count'}):
+                views1 = views.string
+                f1.write("Views: " + views1 + "\n")
+                f1.write("\n \n")
 
-        for link in soup.findAll('a', {'class': 'yt-uix-sessionlink  content-link spf-link        spf-link '}):
-            link1 = "https://www.youtube.com" + link.get('href')
-            links.append(link1)
-            print(link1)
+            for link in soup.findAll('a', {'class': 'yt-uix-sessionlink  content-link spf-link        spf-link '}):
+                link1 = "https://www.youtube.com" + link.get('href')
+                if link1 not in visitedlinks:
+                    links.append(link1)
 
         count += 1
 
 
-crawler(10)
+for url1 in f2.readlines():
+    links.append(url1)
+
+f3.seek(0)
+for url2 in f3.readlines():
+    if url2 not in visitedlinks:
+        visitedlinks.append(url2)
+
+crawler(3)
 f1.close()
 f2.close()
+f3.close()
