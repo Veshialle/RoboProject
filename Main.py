375ba6f00443658b83236613bb2163ae150bbc5d
ciurga93 <big-g@live.it>
2015-12-09 11:07:18 +0100

    v6
    fully working (almost final version). added comments for better
    understanding

--- Main.py
+++ Main.py
@@ -2,60 +2,67 @@ import requests
 from bs4 import BeautifulSoup
 
 
-f1 = open('results.txt', 'w')
-f2 = open('source.txt', 'r')
-f3 = open('seen.txt', 'a+')
+f1 = open('results.txt', 'w')  # file used to store the results
+f2 = open('source.txt', 'a+')  # file used to get the starting url
+f3 = open('seen.txt', 'a+')  # file used to store the visited urls
 links = []
 visitedlinks = []
 
 
 def crawler(maxite):
     count = 0
-    if count == 0:
+    if count == 0:  # == if this is the first iteration
         f1.write("---------- VISITED VIDEOS ----------\n\n")
     for url in links:
-        if count == maxite:
-            print("Done! I visited " + str(maxite) + " videos")
+        if count == maxite:  # if i reached the max number of iterations
+            print("\nDone!")
             break
-        if (url + "\n") not in visitedlinks:
-            print(visitedlinks)
+        if url not in visitedlinks:  # == if i've not seen the video yet
             visitedlinks.append(url)
-            f3.write(url)
-            f3.write("\n")
+            f3.write(url + "\n")
             source_code = requests.get(str(url))
             plain_text = source_code.text
             soup = BeautifulSoup(plain_text, "html.parser")
+
+            # finding the title of the video
             for title in soup.findAll("span", {'id': 'eow-title'}, {'class': 'watch-title'}):
                 title1 = title.get('title')
                 print(title1)
-                f1.write("Title: " + title1 + "\n")
+                f1.write("Title: " + title1 + "\n")  # writing on results.txt
 
+            # finding the date of the video
             for date in soup.findAll("strong", {'class': 'watch-time-text'}):
                 date1 = date.string
-                f1.write("date: " + date1 + "\n")
+                f1.write("date: " + date1 + "\n")  # writing on results.txt
 
+            # finding the number of views of the video
             for views in soup.findAll("div", {'class': 'watch-view-count'}):
                 views1 = views.string
-                f1.write("Views: " + views1 + "\n")
+                f1.write("Views: " + views1 + "\n")  # writing on results.txt
                 f1.write("\n \n")
 
+            # finding all the links of the correlated videos
             for link in soup.findAll('a', {'class': 'yt-uix-sessionlink  content-link spf-link        spf-link '}):
                 link1 = "https://www.youtube.com" + link.get('href')
-                if link1 not in visitedlinks:
-                    links.append(link1)
+                if link1 not in visitedlinks:  # checking if the link has already been visited
+                    links.append(link1)        # if not, add it to the list
+                    f2.write("\n" + link1)       # and write it on the source file
+
+        elif url in visitedlinks:
+            maxite +=1
 
         count += 1
 
-
-for url1 in f2.readlines():
+f2.seek(0)
+for url1 in f2.readlines():  # setting up the list of the urls to be seen
     links.append(url1)
 
-f3.seek(0)
-for url2 in f3.readlines():
+f3.seek(0)  # setting the pointer at the begin of the file (normally using 'a+' mode sets the pointer at the end)
+for url2 in f3.readlines():  # setting up the list of the visited links
     if url2 not in visitedlinks:
         visitedlinks.append(url2)
 
-crawler(3)
-f1.close()
+crawler(5)  # starting the crawler
+f1.close()  # closing streams
 f2.close()
 f3.close()
